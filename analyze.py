7b6a6a07865950e8b8731c0c33af9341a405c0aa
trisha <kotharitrisha@gmail.com>
2013-11-10 22:49:26 -0500

    All files segmented

--- analyze.py
+++ analyze.py
@@ -3,15 +3,13 @@ import csv
 from csv import DictReader
 
 # Constants
-PROD_ID = "prod_category_id"
-title = "machine_id,site_session_id,domain_id,event_date,event_time,prod_category_id,prod_name,prod_qty,prod_totprice,basket_tot,hoh_most_education,census_region,household_size,hoh_oldest_age,household_income,children,racial_background,connection_speed,country_of_origin,zip_code,domain_name"
+PROD_ID = "Product Category ID"
 
 # Function
 def read_my_file(filename):
     with open(filename, 'rb') as fh:
         cread = DictReader(fh)
         categories = {}
-        first_done = False
         print "Users to target: "
         for row_info in cread:
             value = row_info[PROD_ID]
@@ -38,8 +36,8 @@ def read_my_file(filename):
                     vals = []
                     str = ""
                     for k in l.iterkeys():
-                        print k
-                        print l[k]
+                       # print k
+                       # print l[k]
                         str += (l[k])
                         #print k
                         vals.append(l[k])
@@ -50,5 +48,5 @@ def read_my_file(filename):
 # Python note: This if statement checks that we're 
 # running the script and not importing it.
 if __name__ == '__main__':
-    inputfile = 'AmazonWRDS.csv'
+    inputfile = 'Amazon.csv'
     read_my_file(inputfile)
